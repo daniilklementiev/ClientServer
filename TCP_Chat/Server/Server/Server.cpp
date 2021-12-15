d86d7b881fcf6d8cee791b39e813a733b970a5e4
Klementiev Daniil <Klem_em31@student.itstep.org>
2021-12-15 17:47:48 +0200

    123123

--- TCP_Chat/Server/Server/Server.cpp
+++ TCP_Chat/Server/Server/Server.cpp
@@ -1,6 +1,11 @@
-#define _CRT_SECURE_NO_WARNINGS
+#pragma comment (lib, "Ws2_32.lib")
 
+#define _CRT_SECURE_NO_WARNINGS
+#include <WinSock2.h>
+#include <WS2tcpip.h>
 #include <Windows.h>
+#include <wchar.h>
+#include <stdio.h>
 
 #define CMD_START_SERVER	1001
 #define CMD_STOP_SERVER		1002
@@ -9,6 +14,9 @@
 HINSTANCE hInst;
 HWND grpEndpoint, grpLog, serverLog;
 HWND btnStart, btnStop;
+HWND editIP, editPort;
+SOCKET listenSocket;
+
 
 LRESULT CALLBACK WinProc(HWND, UINT, WPARAM, LPARAM);
 DWORD	CALLBACK CreateUI(LPVOID);		// User Interface
@@ -93,11 +101,11 @@ DWORD CALLBACK CreateUI(LPVOID params) {
 		10, 10, 150, 70, hWnd, 0, hInst, NULL);
 	CreateWindowExW(0, L"Static", L"IP:", WS_CHILD | WS_VISIBLE,
 		20, 35, 40, 15, hWnd, 0, hInst, NULL);
-	CreateWindowExW(0, L"Edit", L"127.0.0.1", WS_CHILD | WS_VISIBLE,
+	editIP = CreateWindowExW(0, L"Edit", L"127.0.0.1", WS_CHILD | WS_VISIBLE,
 		45, 35, 110, 15, hWnd, 0, hInst, NULL);
 	CreateWindowExW(0, L"Static", L"Port:", WS_CHILD | WS_VISIBLE,
 		20, 50, 40, 15, hWnd, 0, hInst, NULL);
-	CreateWindowExW(0, L"Edit", L"8888", WS_CHILD | WS_VISIBLE,
+	editPort = CreateWindowExW(0, L"Edit", L"8888", WS_CHILD | WS_VISIBLE,
 		55, 50, 45, 15, hWnd, 0, hInst, NULL);
 
 	grpLog = CreateWindowExW(0, L"Button", L"Server log",
@@ -111,18 +119,91 @@ DWORD CALLBACK CreateUI(LPVOID params) {
 		30, 120, 100, 25, hWnd, (HMENU)CMD_STOP_SERVER, hInst, NULL);
 	EnableWindow(btnStop, FALSE);
 
+	listenSocket = INVALID_SOCKET;
+
 	return 0;
 }
 
 DWORD CALLBACK StartServer(LPVOID params) {
 	HWND hWnd = *((HWND*)params);
-	SendMessageW(serverLog, LB_ADDSTRING, 0, (LPARAM)L"Server start");
+	const size_t MAX_LEN = 100;
+	WCHAR str[MAX_LEN];
+	WSADATA wsaData;
+	int err;
+
+	err = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (err != 0)
+	{
+		_snwprintf_s(str, MAX_LEN, L"Startup failed, error %d", err);
+		WSACleanup();
+		SendMessageW(serverLog, LB_ADDSTRING, 0, (LPARAM)str);
+		return -10;
+	}
+	listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (listenSocket == INVALID_SOCKET)
+	{
+		_snwprintf_s(str, MAX_LEN, L"Socket failed, error %d", WSAGetLastError());
+		WSACleanup();
+		SendMessageW(serverLog, LB_ADDSTRING, 0, (LPARAM)str);
+		return -20;
+	}
+
+	SOCKADDR_IN addr;
+	addr.sin_family = AF_INET;
+	char ip[20];
+	LRESULT ipLen = SendMessageA(editIP, WM_GETTEXT, 19, (LPARAM)ip);
+	ip[ipLen] = '\0';
+	inet_pton(AF_INET, ip, &addr.sin_addr);
+
+	char port[8];
+	LRESULT portLen = SendMessageA(editPort, WM_GETTEXT, 7, (LPARAM)port);
+	port[portLen] = '\0';
+	addr.sin_port = htons(atoi(port));
+
+	err = bind(listenSocket, (SOCKADDR*)&addr, sizeof(addr));
+	if (err == SOCKET_ERROR)
+	{
+		_snwprintf_s(str, MAX_LEN, L"Socket bind, error %d", WSAGetLastError());
+		closesocket(listenSocket);
+		WSACleanup();
+		listenSocket = INVALID_SOCKET;
+		SendMessageW(serverLog, LB_ADDSTRING, 0, (LPARAM)str);
+		return -30;
+	}
+
+	err = listen(listenSocket, SOMAXCONN);
+	if (err == SOCKET_ERROR)
+	{
+		_snwprintf_s(str, MAX_LEN, L"Socket listen, error %d", WSAGetLastError());
+		closesocket(listenSocket);
+		WSACleanup();
+		listenSocket = INVALID_SOCKET;
+		SendMessageW(serverLog, LB_ADDSTRING, 0, (LPARAM)str);
+		return -40;
+	}
+	const size_t ipSize = strlen(ip) + 1;
+	wchar_t* WIP = new wchar_t[ipSize];
+	mbstowcs(WIP, ip, ipSize);
+
+	const size_t portSize = strlen(port) + 1;
+	wchar_t* WPORT = new wchar_t[portSize];
+	mbstowcs(WPORT, port, portSize);
+
+	_snwprintf_s(str, MAX_LEN, L"Server start : %s:%s", WIP, WPORT);
+	SendMessageW(serverLog, LB_ADDSTRING, 0, (LPARAM)str);
 	EnableWindow(btnStart, FALSE);
 	EnableWindow(btnStop, TRUE);
+	delete[] WPORT;
+	delete[] WIP;
 	return 0;
 }
+
 DWORD CALLBACK StopServer(LPVOID params) {
 	HWND hWnd = *((HWND*)params);
+	closesocket(listenSocket);
+	WSACleanup();
+	listenSocket = INVALID_SOCKET;
+
 	SendMessageW(serverLog, LB_ADDSTRING, 0, (LPARAM)L"Server stop");
 	EnableWindow(btnStart, TRUE);
 	EnableWindow(btnStop, FALSE);
